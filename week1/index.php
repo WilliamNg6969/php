cdb6465951d8c78a68a49ce3067d575a6d7d98e6
William <Williamng0227@e.neivce.edu.my>
2024-11-13 13:05:22 +0800

    create a index.php

---
+++ week1/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My first php</title>
+    </head>
+    <body>
+        
+        <?php
+             echo "William ";
+     ?>
+    </body>
+</html>
