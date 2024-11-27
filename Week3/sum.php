dbc00d6c091f45f39d2f9b51879f41cd4235fc8f
William <Williamng0227@e.neivce.edu.my>
2024-11-27 13:23:18 +0800

    Week 4 Handing

---
+++ Week3/sum.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+    <?php
+        
+        
+    $sum = 0;
+        
+    for ($i = 1; $i <= 10; $i++) {
+        $sum += $i; 
+    
+        
+    echo "Sum of first 10 natural numbers is:$sum <br>";
+      
+    }  
+    ?>
+    </body>
+</html>
