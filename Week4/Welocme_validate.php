9c395c01dc277aa038471bc26dfae4dda96400ac
William <Williamng0227@e.neivce.edu.my>
2024-11-27 15:33:33 +0800

    Welcome_validate new

--- Week4/Welocme_validate.php
+++ Week4/Welocme_validate.php
@@ -13,8 +13,14 @@ if (filter_var($_email, FILTER_VALIDATE_EMAIL)) {
     echo "$_email is not a valid email address";
 }
 ?><br>
- Your age: <?php echo $_GET["age"];
- ?>
+ Your age: <?php 
+ $_age = $_GET["age"];
+ if (filter_var($_age, FILTER_VALIDATE_INT, ["options" => ["min_range" => 18, "max_range" => 100]])) {
+    echo "$_age is valid ";
+}else {
+    echo "$_age is not valid ";
+}
+    ?>,
 
 </body>
 </html>
